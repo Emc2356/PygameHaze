fea062e7572a3b6f2d704a317c2a8e7f49e95815
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-07-17 11:03:00 +0300

    moved to the BaseException the main part of a exception so it is not needed to copy-past the code

--- PygameWidgets/exceptions.py
+++ PygameWidgets/exceptions.py
@@ -28,7 +28,12 @@ class BaseException(Exception):
     the base exception for the rest of the exception
     that are used for this package
     """
-    pass
+    def __init__(self, message):
+        self.message = message
+        super().__init__(message)
+
+    def __str__(self):
+        return repr(self.message)
 
 
 class TextOutOfButton(BaseException):
@@ -37,12 +42,7 @@ class TextOutOfButton(BaseException):
     text that is used in the button is out of
     the bounds of the button.
     """
-    def __init__(self, message):
-        self.message = message
-        super().__init__(message)
-
-    def __str__(self):
-        return repr(self.message)
+    pass
 
 
 class InvalidAnchor(BaseException):
@@ -50,12 +50,7 @@ class InvalidAnchor(BaseException):
     the exception that is raised when the
     user provides a invalid anchor for a widget.
     """
-    def __init__(self, message):
-        self.message = message
-        super().__init__(message)
-
-    def __str__(self):
-        return repr(self.message)
+    pass
 
 
 __all__ = [
