a9bdfc86ce028bd5c6d664ccf648e6bf824da7ed
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-07-18 09:46:12 +0300

    Documentation and Example for MultiLineText class

---
+++ PygameWidgets/Example/MultiLineText.py
@@ -0,0 +1,34 @@
+import pygame
+from PygameWidgets import MultiLineText
+from PygameWidgets.constants import *
+
+
+pygame.init()
+pygame.font.init()
+
+
+WIN = pygame.display.set_mode((500, 500))
+
+
+text = MultiLineText(
+    WIN,                                    # WIN
+    250,                                    # x
+    250,                                    # y
+    f"Hello Word{LINE_SPLITTER}Bey World",  # text
+    RED,                                    # color
+    anchor=CENTER,                          # anchor
+    font_size=60,                           # font_size
+    font_type="comicsans",                  # font_type
+    antialias=True                          # antialias
+)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT or event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+            pygame.quit()
+            quit(-1)
+
+    WIN.fill((30, 30, 30))
+    text.draw()
+    pygame.display.update()
