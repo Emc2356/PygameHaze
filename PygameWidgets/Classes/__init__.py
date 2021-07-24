efe31f1de26110054ba10b388bfcc03a7d46d6c8
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-07-24 14:26:43 +0300

    made the PygameWidgets/Classes into a module instead of a normal folder

---
+++ PygameWidgets/Classes/__init__.py
@@ -0,0 +1,6 @@
+from .button import Button
+from .text import SimpleText
+from .text import MultiLineText
+from .input_field import InputField
+from .input_field import InputFieldNumbers
+from .input_field import InputFieldLetters
