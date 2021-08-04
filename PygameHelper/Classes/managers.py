47fb03346d293ebe758f066303077a11cda31787
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-08-04 15:34:45 +0300

    added a InputFieldManager

--- PygameHelper/Classes/managers.py
+++ PygameHelper/Classes/managers.py
@@ -30,6 +30,7 @@ import pygame
 from PygameHelper.constants import *
 from PygameHelper.Classes import Button
 from PygameHelper.Classes import SimpleText, MultiLineText
+from PygameHelper.Classes import InputField, InputFieldNumbers, InputFieldLetters
 
 
 class ButtonManager:
@@ -228,3 +229,121 @@ class TextManager:
     def __reversed__(self) -> List[SimpleText or MultiLineText]:
         reversed(self.texts)
         return self.texts
+
+
+class InputFieldManager:
+    def __init__(self, WIN: pygame.surface.Surface):
+        self.WIN = WIN
+        self.input_fields = []
+        self.__i = 0
+
+    def draw(self) -> None:
+        [text.draw() for text in self.input_fields]
+
+    def update(self) -> None:
+        [text.update() for text in self.input_fields]
+
+    def event_handler(self, event: pygame.event.Event) -> None:
+        [input_field.event_hanler(event) for input_field in self.input_fields]
+
+    def get_input_fields(self) -> List[InputField or InputFieldNumbers or InputFieldLetters]:
+        return self.input_fields
+
+    def add_Input_field(self,
+                        x: int,
+                        y: int,
+                        w: int,
+                        h: int,
+                        base_color: Tuple[int, int, int]=WHITE,
+                        text_color: Tuple[int, int, int]=BLACK,
+                        **kwargs):
+        self.input_fields.append(InputField(self.WIN, x, y, w, h, base_color, text_color, **kwargs))
+
+    def add_Input_field_numbers(self,
+                                x: int,
+                                y: int,
+                                w: int,
+                                h: int,
+                                base_color: Tuple[int, int, int]=WHITE,
+                                text_color: Tuple[int, int, int]=BLACK,
+                                **kwargs):
+        self.input_fields.append(InputFieldNumbers(self.WIN, x, y, w, h, base_color, text_color, **kwargs))
+
+    def add_Input_field_letters(self,
+                                x: int,
+                                y: int,
+                                w: int,
+                                h: int,
+                                base_color: Tuple[int, int, int] = WHITE,
+                                text_color: Tuple[int, int, int] = BLACK,
+                                **kwargs):
+        self.input_fields.append(InputFieldLetters(self.WIN, x, y, w, h, base_color, text_color, **kwargs))
+
+    def __getitem__(self, item) -> InputField or InputFieldNumbers or InputFieldLetters:
+        return self.input_fields[item]
+
+    def __setitem__(self, key, value) -> None:
+        self.input_fields[key] = value
+
+    def __delitem__(self, key) -> None:
+        del self.input_fields[key]
+
+    def __iadd__(self, other) -> None:
+        if isinstance(other, InputFieldManager):
+            self.input_fields += other.input_fields
+        else:
+            raise TypeError(f"the given obj is not a instance of {InputFieldManager} and it is a instance of the class {type(other)}")
+
+    def __add__(self, other) -> None:
+        if isinstance(other, InputFieldManager):
+            self.input_fields += other.input_fields
+        else:
+            raise TypeError(f"the given obj is not a instance of {InputFieldManager} and it is a instance of the class {type(other)}")
+
+    def __contains__(self, item) -> bool:
+        if item in self.input_fields:
+            return True
+        return False
+
+    def __del__(self) -> None:
+        for text in self.input_fields:
+            del text
+
+    def __len__(self) -> int:
+        return len(self.input_fields)
+
+    def __iter__(self) -> Iterable[InputField or InputFieldNumbers or InputFieldLetters]:
+        return iter(self.input_fields)
+
+    def __next__(self) -> InputField or InputFieldNumbers or InputFieldLetters:
+        try:
+            item = self.input_fields[self.__i]
+            self.__i += 1
+        except IndexError:
+            self.__i = 0
+            item = self.__next__()
+
+        return item
+
+    def __repr__(self) -> str:
+        _str = "["
+        for text in self.input_fields:
+            _str += f"{text},\n"
+        _str = _str[:-2]
+        _str += "]"
+        return _str
+
+    def __str__(self) -> str:
+        _str = "["
+        for text in self.input_fields:
+            _str += f"{text},\n"
+        _str = _str[:-2]
+        _str += "]"
+        return _str
+
+    def __bool__(self) -> bool:
+        return len(self) > 0
+
+    def __reversed__(self) -> List[InputField or InputFieldNumbers or InputFieldLetters]:
+        reversed(self.input_fields)
+        return self.input_fields
