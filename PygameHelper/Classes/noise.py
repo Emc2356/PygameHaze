ee07b0e3832010b5328f42fe742cf0324f0a51ec
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-11-05 13:07:10 +0200

    fixed `noise` (it was making an array of 4096 in every call)

--- PygameHelper/Classes/noise.py
+++ PygameHelper/Classes/noise.py
@@ -27,8 +27,6 @@ perlin noise
 
 
 from typing import Union
-import random
-import numpy as np
 import math
 
 
@@ -37,74 +35,69 @@ NumType = Union[int, float]
 
 class _PerlinNoise:
     def __init__(self):
-        self.PERLIN_YWRAPB = 4
-        self.PERLIN_YWRAP = 1 << self.PERLIN_YWRAPB
-        self.PERLIN_ZWRAPB = 8
-        self.PERLIN_ZWRAP = 1 << self.PERLIN_ZWRAPB
-        self.PERLIN_SIZE = 4095
-        self.octaves = 4  # default to medium smooth
-        self.perlin_amp_falloff = 0.5  # 50% reduction/octave
-        self.perlin = None
+        self._PERLIN_YWRAPB = 4
+        self._PERLIN_YWRAP = 1 << self._PERLIN_YWRAPB
+        self._PERLIN_ZWRAPB = 8
+        self._PERLIN_ZWRAP = 1 << self._PERLIN_ZWRAPB
+        self._PERLIN_SIZE = 4095
+        self._octaves = 4  # default to medium smooth
+        self._perlin_amp_falloff = 0.5  # 50% reduction/octave
+        self._perlin = None  # lazy load it as some people don't use it
 
-    def noise_detail(self, octaves: int=-1, falloff: float=-1) -> None:
+    def detail(self, octaves: int=-1, falloff: float=-1) -> None:
         """
         its sets the number of octaves that are going to be used and the falloff factor for each octave
         :param octaves: int=-1
-        :param falloff: int=-1
+        :param falloff: float=-1
         :return:
         """
         if octaves > 0:
-            self.octaves = octaves
+            self._octaves = octaves
         if falloff > 0:
-            self.perlin_amp_falloff = falloff
+            self._perlin_amp_falloff = falloff
 
     @staticmethod
     def _scaled_cosine(i: NumType) -> NumType:
         return 0.5 * (1.0 - math.cos(i * math.pi))
 
     def __call__(self, x: NumType, y: NumType=0, z: NumType=0) -> NumType:
-        if self.perlin is None:
-            self._perlin = np.ndarray([self.PERLIN_SIZE + 1])
-            for i in range(self.PERLIN_SIZE + 1):
-                self._perlin[i] = random.random()
+        if self._perlin is None:
+            import numpy as np
+            self._perlin = np.random.random((self._PERLIN_SIZE + 1,))
 
-        if x < 0: x = -x
-        if y < 0: y = -y
-        if z < 0: z = -z
+        xi = math.floor(abs(x))
+        yi = math.floor(abs(y))
+        zi = math.floor(abs(z))
+        xf = abs(x) - xi
+        yf = abs(y) - yi
+        zf = abs(z) - zi
 
-        xi = math.floor(x)
-        yi = math.floor(y)
-        zi = math.floor(z)
-        xf = x - xi
-        yf = y - yi
-        zf = z - zi
-
-        r = 0
+        value = 0
         ampl = 0.5
 
-        for o in range(self.octaves):
-            of = xi + (yi << self.PERLIN_YWRAPB) + (zi << self.PERLIN_ZWRAPB)
+        for _ in range(self._octaves):
+            of = xi + (yi << self._PERLIN_YWRAPB) + (zi << self._PERLIN_ZWRAPB)
 
             rxf = self._scaled_cosine(xf)
             ryf = self._scaled_cosine(yf)
 
-            n1 = self._perlin[of & self.PERLIN_SIZE]
-            n1 += rxf * (self._perlin[(of + 1) & self.PERLIN_SIZE] - n1)
-            n2 = self._perlin[(of + self.PERLIN_YWRAP) & self.PERLIN_SIZE]
-            n2 += rxf * (self._perlin[(of + self.PERLIN_YWRAP + 1) & self.PERLIN_SIZE] - n2)
+            n1 = self._perlin[of & self._PERLIN_SIZE]
+            n1 += rxf * (self._perlin[(of + 1) & self._PERLIN_SIZE] - n1)
+            n2 = self._perlin[(of + self._PERLIN_YWRAP) & self._PERLIN_SIZE]
+            n2 += rxf * (self._perlin[(of + self._PERLIN_YWRAP + 1) & self._PERLIN_SIZE] - n2)
             n1 += ryf * (n2 - n1)
 
-            of += self.PERLIN_ZWRAP
-            n2 = self._perlin[of & self.PERLIN_SIZE]
-            n2 += rxf * (self._perlin[(of + 1) & self.PERLIN_SIZE] - n2)
-            n3 = self._perlin[(of + self.PERLIN_YWRAP) & self.PERLIN_SIZE]
-            n3 += rxf * (self._perlin[(of + self.PERLIN_YWRAP + 1) & self.PERLIN_SIZE] - n3)
+            of += self._PERLIN_ZWRAP
+            n2 = self._perlin[of & self._PERLIN_SIZE]
+            n2 += rxf * (self._perlin[(of + 1) & self._PERLIN_SIZE] - n2)
+            n3 = self._perlin[(of + self._PERLIN_YWRAP) & self._PERLIN_SIZE]
+            n3 += rxf * (self._perlin[(of + self._PERLIN_YWRAP + 1) & self._PERLIN_SIZE] - n3)
             n2 += ryf * (n3 - n2)
 
             n1 += self._scaled_cosine(zf) * (n2 - n1)
 
-            r += n1 * ampl
-            ampl *= self.perlin_amp_falloff
+            value += n1 * ampl
+            ampl *= self._perlin_amp_falloff
             xi <<= 1
             xf *= 2
             yi <<= 1
@@ -122,7 +115,7 @@ class _PerlinNoise:
                 zi += 1
                 zf -= 1
 
-        return r
+        return value
 
 
 noise = _PerlinNoise()
