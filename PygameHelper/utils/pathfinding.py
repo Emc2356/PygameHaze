d049be727634effbd4016f4643bfd57f01d98702
Emc2356 <63981925+Emc2356@users.noreply.github.com>
2021-11-13 16:17:10 +0200

    removed `BFS` pathfinding

--- PygameHelper/utils/pathfinding.py
+++ PygameHelper/utils/pathfinding.py
@@ -31,16 +31,16 @@ import queue
 
 
 Number = Union[int, float]
-NeighborOutputType = TypeVar("NeighborOutputType")
 
 
 class _ASPos:  # it is used for A* pathfinding
-    __slots__ = "i", "j", "v"
+    __slots__ = "i", "j", "v", "tp"
 
     def __init__(self, idxs: Tuple[int, int], v: Number) -> None:
-        self.i: int = idxs[0]
-        self.j: int = idxs[1]
+        self.i: int = (idxs[0])
+        self.j: int = (idxs[1])
         self.v: Number = v
+        self.tp: Tuple[int, int] = (self.i, self.j)
 
     @property
     def pos(self) -> Tuple[int, int]:
@@ -67,139 +67,70 @@ class _ASPos:  # it is used for A* pathfinding
     def __lt__(self, other):
         return False
 
-    @TpOverload
-    def __getitem__(self, idx: int) -> int: ...
-
-    @TpOverload
-    def __getitem__(self, idx: slice) -> List[int]: ...
-
-    def __getitem__(self, idx: slice) -> Union[int, List[int]]:
-        return [self.i, self.j, self.v][idx]
-
     def __repr__(self) -> str:
         return repr(f"${self.i} ${self.j} ${self.v}")
 
 
-def AS_heuristic(p1: _ASPos, p2: _ASPos) -> int:  # it is used for A* pathfinding
-    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
-
-
-def bfs_found_end(path: str, start: Tuple[int, int], end: Tuple[int, int]) -> bool:
-    i: int = start[0]
-    j: int = start[1]
-    for move in path:
-        if move in "LR": j += bfs_values[move]
-        else: i += bfs_values[move]
-
-    if (i, j) == end:
-        return True
-    return False
-
-
-def bfs_valid(grid: List[List[int]], path: str, start: Tuple[int, int]) -> bool:
-    i: int = start[0]
-    j: int = start[1]
-    for move in path:
-        if move in "LR": j += bfs_values[move]
-        else: i += bfs_values[move]
-        if not(0 <= i < len(grid) and 0 <= j < len(grid[0])):
-            return False
-        elif grid[i][j] != 0:
-            return False
-    return True
-
-
-bfs_opposites: Dict[str, str] = {"L": "R", "R": "L", "U": "D", "D": "U", "": "werg"}
-bfs_values: Dict[str, int] = {"L": -1, "R": 1, "U": -1, "D": 1, "": 0}
+def AS_heuristic(p1: _ASPos, p2: _ASPos) -> Number:  # it is used for A* pathfinding
+    return int(abs(p1.i - p2.i) + abs(p1.j - p2.j))
 
 
 def pathfinding(
         grid: List[List[int]], start: Union[List[int], Tuple[int, int], Sequence[int]],
-        end: Union[List[int], Tuple[int, int], Sequence[int]], algorithm: str = "A*"
-) -> List[Tuple[int, int]]:
+        end: Union[List[int], Tuple[int, int], Sequence[int]]
+) -> Sequence[Tuple[float, float]]:
     """
-    if finds the most efficient path from 1 point to another
+    if finds the most efficient path from one point to another
     if the value of the grid is 0 then the algorithm can go there if it is something over 0 then the algorithm considers ot a wall
-    :param start: the start
-    :param end: the goal
+    :param start: Sequence[int]
+    :param end: Sequence[int]
     :param grid: List[List[int]]
-    :param algorithm: str="A*"
     :return: List[Tuple[int, int]]
     """
-    if algorithm.lower() == "a*":
-        columns: int = len(grid)
-        rows: int = len(grid[0])
+    ASPGrid: List[List[_ASPos]] = [[_ASPos((i, j), grid[i][j]) for j in range(len(grid[0]))] for i in range(len(grid))]
 
-        grid: List[List[_ASPos]] = [[_ASPos((i, j), grid[i][j]) for j in range(rows)] for i in range(columns)]
+    ASPstart: _ASPos = ASPGrid[start[0]][start[1]]
+    ASPend: _ASPos = ASPGrid[end[0]][end[1]]
 
-        start: _ASPos = grid[start[0]][start[1]]
-        end: _ASPos = grid[end[0]][end[1]]
+    count: int = 0
+    open_set: queue.PriorityQueue = queue.PriorityQueue()
+    open_set.put((0, count, ASPstart))
+    open_set_hash: Set[_ASPos] = {ASPstart}
 
-        count: int = 0
-        open_set: queue.PriorityQueue = queue.PriorityQueue()
-        open_set.put((0, count, start))
+    came_from: Dict = {}
 
-        came_from: Dict = dict()
+    g_score: Dict[_ASPos, float] = {pos: float("inf") for row in ASPGrid for pos in row}
+    g_score[ASPstart] = 0
+    f_score: Dict[_ASPos, float] = {pos: float("inf") for row in ASPGrid for pos in row}
+    f_score[ASPstart] = AS_heuristic(ASPstart, ASPend)
 
-        g_score: Dict[_ASPos, float] = {pos: float("inf") for row in grid for pos in row}
-        g_score[start] = 0
-        f_score: Dict[_ASPos, float] = {pos: float("inf") for row in grid for pos in row}
-        f_score[start] = AS_heuristic(start, end)
+    current: _ASPos
+    while not open_set.empty():  # could be replaced with while open_set.qsize():
+        # get the best spot that we have available
+        current = open_set.get()[2]
+        open_set_hash.remove(current)
 
-        open_set_hash: Set[_ASPos] = {start}
+        if current is ASPend:
+            path = []
+            while current in came_from:
+                current = came_from[current]
+                path.append(current.tp)
+            path.reverse()
+            return path
 
-        while not open_set.empty():  # could be replaced with while open_set.qsize():
-            # get the best spot that we have available
-            current: _ASPos = open_set.get()[2]
-            open_set_hash.remove(current)
+        for neighbor in current.neighbors(ASPGrid):  # type: _ASPos
+            temp_g_score = g_score[current] + 1
 
-            if current is end:
-                path = []
-                while current in came_from:
-                    current = came_from[current]
-                    path.append(tuple(current[0:2]))
-                path.reverse()
-                return path
+            if temp_g_score < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = temp_g_score
+                f_score[neighbor] = temp_g_score + AS_heuristic(neighbor, ASPend)
+                if neighbor not in open_set_hash:
+                    count += 1
+                    open_set.put((f_score[neighbor], count, neighbor))
+                    open_set_hash.add(neighbor)
 
-            for neighbor in current.neighbors(grid):  # type: _ASPos
-                temp_g_score = g_score[current] + 1
-
-                if temp_g_score < g_score[neighbor]:
-                    came_from[neighbor] = current
-                    g_score[neighbor] = temp_g_score
-                    f_score[neighbor] = temp_g_score + AS_heuristic(neighbor, end)
-                    if neighbor not in open_set_hash:
-                        count += 1
-                        open_set.put((f_score[neighbor], count, neighbor))
-                        open_set_hash.add(neighbor)
-
-        return []
-    elif algorithm.lower() == "bfs":
-        moves: List[str] = [""]
-        current: str = ""
-
-        start: Tuple[int, int] = tuple(start)
-        end: Tuple[int, int] = tuple(end)
-
-        while not bfs_found_end(current, start, end):
-            current: str = moves.pop(0)
-            for j in ["L", "R", "U", "D"]:
-                if not bfs_opposites[current[len(current)-1:]] == j: to_put: str = current + j
-                else: to_put = current
-                if bfs_valid(grid, to_put, start):
-                    moves.append(to_put)
-
-            moves = list(sorted(set(moves), key=moves.index))
-
-        path: List[Tuple[int, int]] = [start]
-        for move in current:
-            if   move == "L": path.append((path[~0][0], path[~0][1] - 1))
-            elif move == "R": path.append((path[~0][0], path[~0][1] + 1))
-            elif move == "U": path.append((path[~0][0] - 1, path[~0][1]))
-            elif move == "D": path.append((path[~0][0] + 1, path[~0][1]))
-        return path[1:]
-    else:
-        raise TypeError(f"unrecognised pathfinding algorithm '{algorithm}'. Possible algorithms: A* and BFS")
+    return []
 
 
 __all__ = ["pathfinding"]
