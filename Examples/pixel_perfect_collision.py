78dacace185ce724322cf3370e41cf69cb32dc36
Axis <77634274+blankRiot96@users.noreply.github.com>
2021-12-10 18:43:30 +0530

    Small change in `pixel_perfect_collision.py`
    I just felt that using the functions you have provided `load_image` and `load_alpha_image` would be better as an example of the usage of this library rather than doing `pygame.image.load`

--- Examples/pixel_perfect_collision.py
+++ Examples/pixel_perfect_collision.py
@@ -14,14 +14,14 @@ FPS = 120
 pygame.display.set_caption("Pixel perfect collision")
 
 
-obstacle = pygame.image.load("assets/obstacle.png").convert_alpha()
+obstacle = pgh.load_image("assets/obstacle.png")
 obstacle_mask = pygame.mask.from_surface(obstacle)
 obstacle_rect = obstacle.get_rect()
 ox = W//2 - obstacle_rect.center[0]
 oy = H//2 - obstacle_rect.center[1]
 
-green_blob = pygame.image.load("assets/green_blob.png").convert_alpha()
-orange_blob = pygame.image.load("assets/orange_blob.png").convert_alpha()
+green_blob = pgh.load_alpha_image("assets/green_blob.png")
+orange_blob = pgh.load_alpha_image("assets/orange_blob.png")
 blob_mask = pygame.mask.from_surface(green_blob)
 blob_color = green_blob
 
